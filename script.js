fe8269ae9579e6267860abe98c144df2be7787cf
kevinakadamian19 <kevindamianta@gmail.com>
2019-05-19 23:07:27 -0700

    Finished!

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //Below is an array of objects that indicate quesiton #, question, and answer choices
 const quizQuestions = [
 		{
-			number: 1,
+			number: 0,
 			question: `Which of these pokemon is not legendary?`,
 			choice1: 'Kyogre',
 			choice2: 'Reshiram',
@@ -12,7 +12,7 @@ const quizQuestions = [
 		},
 
 		{
-			number: 2,
+			number: 1,
 			question: `In what tower can the pokemon Ho Oh be found in?`,
 			choice1: `Burned Tower`,
 			choice2: `Scorced Tower`,
@@ -21,7 +21,7 @@ const quizQuestions = [
 		},
 
 		{
-			number: 3,
+			number: 2,
 			question: `How many total pokemon are there currently in the Pokedex?`,
 			choice1: 782,
 			choice2: 950,
@@ -30,7 +30,7 @@ const quizQuestions = [
 		},
 
 		{
-			number: 4,
+			number: 3,
 			question: `What type of Pokemon is Flygon?`,
 			choice1: `Dragon/Flying`,
 			choice2: `Ground/Flying`,
@@ -39,7 +39,7 @@ const quizQuestions = [
 		},
 
 		{
-			number: 5,
+			number: 4,
 			question: `In total how many different types of pokemon are out there?`,
 			choice1: 14,
 			choice2: 19,
@@ -48,7 +48,7 @@ const quizQuestions = [
 		},
 
 		{
-			number: 6,
+			number: 5,
 			question: `What is another name that can be used to refer to the Suicune, Raikou, and Entei?`,
 			choice1: `Legendary Beasts`,
 			choice2: `Lake Guardians`,
@@ -61,19 +61,51 @@ const quizQuestions = [
 const answerKey = [
 	'Lucario',
 	`Burned Tower`,
-	807,
+	'807',
 	`Ground/Dragon`,
-	18,
+	'18',
 	`Legendary Beasts`
 ];
+
+//Two variables below are what will be used to show current question count, and total correct answers.
+let questionCount = 0;
+let correctCount = 0;
+
+//Below is a function that will be used to insert the questiont template into the DOM.
+function questionFormat() {
+	let questionTemplate = `
+			<form class="question-form">
+				<h1>${quizQuestions[questionCount].question}</h1>
+				<fieldset>
+				<label>
+					<input type="radio" name="answer" value="${quizQuestions[questionCount].choice1}" required>
+					<span>${quizQuestions[questionCount].choice1}</span>
+				</label>
+				<label>
+					<input type="radio" name="answer" value="${quizQuestions[questionCount].choice2}" required>
+					<span>${quizQuestions[questionCount].choice2}</span>
+				</label>
+				<label>
+					<input type="radio" name="answer" value="${quizQuestions[questionCount].choice3}" required>
+					<span>${quizQuestions[questionCount].choice3}</span>
+				</label>
+				<label>
+					<input type="radio" name="answer" value="${quizQuestions[questionCount].choice4}" required>
+					<span>${quizQuestions[questionCount].choice4}</span>
+				</label>	
+				</fieldset>
+				<button type="submit" class="submitButton">Submit Answer</button>
+			</form>`;
+	return questionTemplate;
+}
 
 //Below are the two functions for the correct, and incorrect feedback template
 
 function correctFeedback() {
 	const correctTemplate = `
 	
-		<div class="got-it-right" id="feedback">
-    		<h1>Nice! You got it right!</h1>
+		<div class="right feedback">
+    		<h1>Nice! ${answerKey[questionCount]} is the right answer!</h1>
     		<img src="pikachu.png" alt="A picture of a happy picture">
     		<button type="button" class="nextButton">Continue</button>
 		</div>`;
@@ -83,43 +115,12 @@ function correctFeedback() {
 function incorrectFeedback() {
 	const incorrectTemplate = `
 
-		<div class="got-it-wrong" id="feedback">
-			<h1>Oops! The correct answer is</h1>
+		<div class="wrong feedback">
+			<h1>Oops! The correct answer is ${answerKey[questionCount]}</h1>
     		<img src="evee.png" alt="A picture of an Evee looking confused.">
 			<button type="button" class="nextButton">Continue</button>
 		</div>`;
 	return incorrectTemplate;
-}
-
-//Two variables below are what will be used to show current question count, and total correct answers.
-let questionCount = 0;
-let correctCount = 0;
-
-//Below is a function that will be used to insert the questiont template into the DOM.
-function questionFormat() {
-	let questionTemplate = `<h2>Which of these pokemon is not legendary?</h2>
-			<form class="question-form">
-				<fieldset>
-				<label>
-					<input type="radio" name="answer" required>
-					<span>Kyogre</span>
-				</label>
-				<label>
-					<input type="radio" name="answer" required>
-					<span>Reshiram</span>
-				</label>
-				<label>
-					<input type="radio" name="answer" required>
-					<span>Lucario</span>
-				</label>
-				<label>
-					<input type="radio" name="answer" required>
-					<span>Regigigas</span>
-				</label>	
-				</fieldset>
-				<button type="submit" class="submitButton">Submit Answer</button>
-			</form>`;
-	return questionTemplate;
 }
 
 //This function will be to start the quiz switching from start screen to question screen.
@@ -128,56 +129,83 @@ function beginQuiz() {
 	$('.start-quiz').on('click', '#start-button', function(event) {
 		$('.start-quiz').remove();
 		$('.main-box').append(questionFormat());
+		$('#question-number').text(questionCount+1);
 	});
 }
 
 //This function will increase the displayed Question count
 function increaseQuestionCount() {
+	questionCount++;
+	$('#question-number').text(questionCount+1);
+}
 
+//This function will increase the score displayed if answered correctly.
+function increaseCorrectCount() {
+	correctCount++;
+	$('#correct-questions').text(correctCount);
+	
 }
 
 //This function will be for submittin an answer to one of the questions.
 function submitAnswer() {
-	$('.question-form').on('submit', '.submitButton', function(event) {
+	$('.main-box').on('submit', function(event) {
 		event.preventDefault();
 		let submission = $('input:checked');
 		let answer = submission.val();
-		let correct = 'Lucario';
-		if (answer === correct) {
+		let correctAnswer = answerKey[questionCount];
+		if (answer === correctAnswer) {
 			return answerCorrect();
 		}
 		else {
 			return answerIncorrect();
 		}
-		//Increase questionCount
 	});
 }
 
 //This function will be for when a user answers a question correctly.
 function answerCorrect() {
-
+	increaseCorrectCount();
+ 	$('form').remove();
+ 	$('.main-box').append(correctFeedback());
 }
 
 //This function will be for when a user answers a question incorrectly.
 function answerIncorrect() {
-// return incorrectFeedback();
+	$('form').remove();
+ 	$('.main-box').append(incorrectFeedback());
+
 }
 
 //This function will be to move on to the next question in the quiz after the feedback screen.
 function nextQuestion() {
-
+	$('.main-box').on('click', '.nextButton', function(event) {
+		let totalQuestions = (quizQuestions.length)-1;
+		if (questionCount < totalQuestions) {
+		increaseQuestionCount();
+		$('.feedback').remove();
+		$('.main-box').append(questionFormat());
+		}
+		else {
+			$('.feedback').remove();
+			$('.main-box').append(quizResults());
+		}
+		console.log(questionCount);
+	});
 }
 
 //This function is for the end of the quiz to display the total results.
 function quizResults() {
-
+	let results = `
+	<div class="results">
+		<h1>The results are in! You answered<span id="correct-questions"> ${correctCount}</span> right!</h1>
+		<button value="Refresh Page" onClick="window.location.reload();">Try Again?</button>
+	</div>`;
+	return results;
 }
 
 function renderQuiz() {
 	beginQuiz();
 	submitAnswer();
-	answerCorrect();
-	answerIncorrect();
 	nextQuestion();
 	quizResults();
 }
